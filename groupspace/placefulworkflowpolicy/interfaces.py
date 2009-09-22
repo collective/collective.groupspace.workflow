4bdb824bcec37a91f7ae9ca833c2b1b40aa697c1
Maik Röder <maikroeder@gmail.com>
2009-09-22 10:14:43 +0000

    =Adding an interface that signals the creation of a local workflow policy
    git-svn-id: http://svn.plone.org/svn/collective/groupspace.placefulworkflowpolicy/trunk@97984 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ groupspace/placefulworkflowpolicy/interfaces.py
@@ -0,0 +1,11 @@
+from zope.interface import Interface
+
+class ILocalGroupSpaceWorkflow(Interface):
+    """
+    ILocalGroupSpaceWorkflow interface
+
+    When this interface is implemented on a GroupSpace, a local workflow policy
+    is added when the object is created. This is a special workflow policy
+    that makes sure the default workflow is really chosen, and differs from
+    the default workflow policy in CMFPlacefulWorkflow in this respect. 
+    """                                                  
